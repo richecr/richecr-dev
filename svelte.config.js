ee682ad7eb28105b4106299c99b64643003cc25f
Rich Ramalho <richecr1799@gmail.com>
2024-04-27 18:02:43 -0300

    fix name

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/richecr.github.io';
+const base = '/richecr-portofolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
