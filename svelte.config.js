8eb79cff75cc27c710087f5823a7cd7e7f1ec6c0
Rich Ramalho <richecr1799@gmail.com>
2025-08-24 15:47:46 -0300

    Change base path from '/richecr-portofolio' to '/richecr-dev'

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/richecr-portofolio';
+const base = '/richecr-dev';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
